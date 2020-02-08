f16684b9716389a3ec1e3464e8c8e4d8863ccbcb
Ivan Zaitsev <ivan909020@gmail.com>
2020-02-08 13:52:28 +0200

    The HibernateFactory class has been refactored

--- telegram-bot/src/main/java/ua/ivan909020/bot/repositories/impl/HibernateFactory.java
+++ telegram-bot/src/main/java/ua/ivan909020/bot/repositories/impl/HibernateFactory.java
@@ -5,25 +5,29 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import ua.ivan909020.bot.domain.entities.*;
 
-class HibernateFactory {
+final class HibernateFactory {
 
-    private static SessionFactory sessionFactory;
+    private static final SessionFactory SESSION_FACTORY = createSessionFactory();
+
+    private HibernateFactory() {
+    }
+
+    private static SessionFactory createSessionFactory() {
+        Configuration configuration = new Configuration();
+        configuration.configure();
+        configuration.addAnnotatedClass(Category.class);
+        configuration.addAnnotatedClass(Client.class);
+        configuration.addAnnotatedClass(Order.class);
+        configuration.addAnnotatedClass(OrderItem.class);
+        configuration.addAnnotatedClass(OrderState.class);
+        configuration.addAnnotatedClass(Product.class);
+        StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder();
+        builder.applySettings(configuration.getProperties());
+        return configuration.buildSessionFactory(builder.build());
+    }
 
     static SessionFactory getSessionFactory() {
-        if (sessionFactory == null) {
-            Configuration configuration = new Configuration();
-            configuration.configure();
-            configuration.addAnnotatedClass(Category.class);
-            configuration.addAnnotatedClass(Client.class);
-            configuration.addAnnotatedClass(Order.class);
-            configuration.addAnnotatedClass(OrderItem.class);
-            configuration.addAnnotatedClass(OrderState.class);
-            configuration.addAnnotatedClass(Product.class);
-            StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder();
-            builder.applySettings(configuration.getProperties());
-            sessionFactory = configuration.buildSessionFactory(builder.build());
-        }
-        return sessionFactory;
+        return SESSION_FACTORY;
     }
 
 }
